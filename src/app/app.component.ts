5d681ec5313732847b9ced11aef9c8bca227859e
Andreas Lorenz <info@andreaslorenz.tech>
2025-07-15 11:47:43 +0200

    Implement Cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface Product {
-    id: number;
-    name: string;
-    description: string;
-    price: number;
-  }
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -16,6 +16,7 @@ interface Product {
 export class AppComponent implements OnInit {
   products: Product[] = [];
   cart: Product[] = [];
+  showCart = false;
 
   constructor(private http: HttpClient) {}
 
@@ -33,7 +34,29 @@ export class AppComponent implements OnInit {
     this.cart.push(product);
   }
 
+  toggleCart() {
+    this.showCart = !this.showCart;
+  }
+
   get cartCount(): number {
     return this.cart.length;
+  }
+
+  get groupedCartItems(): { product: Product; quantity: number }[] {
+    const map = new Map<number, { product: Product; quantity: number }>();
+
+    this.cart.forEach(p => {
+      if (map.has(p.id)) {
+        map.get(p.id)!.quantity += 1;
+      } else {
+        map.set(p.id, { product: p, quantity: 1 });
+      }
+    });
+
+    return Array.from(map.values());
+  }
+
+  get totalCartPrice(): number {
+    return this.cart.reduce((sum, item) => sum + item.price, 0);
   }
 }
